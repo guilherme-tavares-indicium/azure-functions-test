014e6513ea4fd092c365c298ecfa666c0dbc1f9d
Guilherme Tavares <guilherme.tavares@indicium.tech>
2023-03-30 10:56:14 -0300

    add local output file to .gitignore

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from datetime import datetime
 now = datetime.now() # current date and time
 connectionString = os.environ.get("CONNECTION_STRING")
 containerName = "output"
-outputBlobName	= f"iris_setosa_v3_{now}.csv"
+outputBlobName	= f"iris_setosa_{now}.csv"
 
 # Establish connection with the blob storage account
 blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)
 
-# Load iris dataset from the task node
+# Load iris dataset
 df = pd.read_csv("iris.csv")
 
 # Take a subset of the records
