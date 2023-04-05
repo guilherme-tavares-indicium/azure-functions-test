a4e0a8f48a56da94acfc33228b484fe690fa7c8a
Guilherme Tavares <guilherme.tavares@indicium.tech>
2023-04-05 17:02:18 -0300

    bad commit for testing purposes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ outputBlobName	= f"iris_setosa_v4_{now}.csv"
 blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)
 
 # Load iris dataset
-df = pd.read_csv("iris.csv")
+df = pd.read_csv("iriss.csv")
 
 # Take a subset of the records
 df = df[df['Species'] == "setosa"]
